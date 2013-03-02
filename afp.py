b358a2b6faebfd26da7474abedcf1bcb1d823c7c
Andrés Reyes Monge <andres@guegue.net>
2013-03-02 11:26:18 -0600

    Trabajando en afp, falta mover las fotos y cambiar las referencias a ellas en el texto

--- afp.py
+++ afp.py
@@ -1,20 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import argparse
 import os
 import redis
 from defusedxml.minidom import parse
-import psycopg2
 import datetime
 
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="directorio de archivos xml")
+args = parser.parse_args()
 
 def get_filelist(path):
-    return [os.path.join(group[0], filename)
+    return (os.path.join(group[0], filename)
         for  group in os.walk(path)
         for filename in group[2]
-        if filename.endswith('.xml') and not filename == 'index.xml']
+        if filename.endswith('.xml') and not filename == 'index.xml')
 
 
 
-def process_news_file(file_path, rconn):
+def process_news_file(file_path):
+    rconn = redis.Redis()
     if rconn.sismember('news:files', file_path):
         return
 
@@ -29,21 +34,44 @@ def process_news_file(file_path, rconn):
     rconn.hdel('news:lock', file_path)
     rconn.sadd('news:files', file_path)
 
+    return news_items
+
 def process_news_item(news_item):
-    print news_item
+    data = {}
+    for node in news_item.childNodes:
+        if node.localName == 'Identification':
+            date_elem = node.getElementsByTagName('DateId')[0]
+            data['date'] = datetime.datetime.strptime(date_elem.firstChild.nodeValue, '%Y%m%d')
+
+        if node.localName == 'NewsComponent':
+            # Trabajando en obtener el titulo y el slug
+            newslines_node = [n for n in node.childNodes if n.localName == 'NewsLines'][0]
+            headline_node = [c for c in newslines_node.childNodes if c.localName == 'HeadLine'][0]
+            text = headline_node.firstChild.nodeValue
+
+            try:
+                slug_node = [c for c in newslines_node.childNodes if c.localName == 'SlugLine'][0]
+                data['slug'] = slug_node.firstChild.nodeValue
+
+            except IndexError:
+                pass
+
+            data['title'] = text
+
+
+            #Procesando el texto
+            news_component_nodes = [n for n in node.childNodes if n.localName == 'NewsComponent']
+            text_news_component_node = [n for n in news_component_nodes
+                                        if 'ContentItem' in map(lambda x: x.localName, n.childNodes)  ][0]
+
+            data['content'] = text_news_component_node.getElementsByTagName('DataContent')[0].toxml()
+
+
+
+    return data
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="directorio de archivos xml")
-    args = parser.parse_args()
-    directory = args.path
 
-    rconn = redis.Redis("localhost")
-
-    map(lambda file_path: process_news_file(file_path, rconn), get_filelist(directory))
-
-    conn = psycopg2.connect("dbname=laprensa user=laprensa")
-    cursor = conn.cursor()
-    cursor.close()
-    conn.close()
+    import pprint
+    pprint.pprint(map(process_news_file, get_filelist(args.path)))
