ee26595f32ae25e6a73477f51e264e6e2ece75fd
Alvarez Velez, Juan Carlos <juan.alvarez@laprensa.com.ni>
2013-10-31 14:00:56 -0600

    use settings

--- sync.sh
+++ sync.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #this script use s3cmd
-S3_BUCKET="s3.laprensa.com.ni"
-S3_KEY_DEST="/home/juancarlos/laprensa/afp/"
-S3_CONFIG_FILE="/home/alvarezjuan/.s3cfg"
-ORIGIN_PATH="/home/alvarezjuan/www/lp_python_apps/afp-laprensa/AFP/espanol/periodico/mundoya/"
 
-
+. ./settings.py
 for file in $ORIGIN_PATH*.jpg
 do
 s3cmd --skip-existing --config=$S3_CONFIG_FILE -P put $file s3://$S3_BUCKET$S3_KEY_DEST
