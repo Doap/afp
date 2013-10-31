2fe10eb1d5dbcbe0832f48a813adf3e8dda9e884
Alvarez Velez, Juan Carlos <juan.alvarez@laprensa.com.ni>
2013-10-31 15:53:48 -0600

    setting file

---
+++ settings.py
@@ -0,0 +1,9 @@
+S3_BUCKET="s3.laprensa.com.ni"
+S3_KEY_DEST="/home/juancarlos/laprensa/afp/"
+S3_CONFIG_FILE="/home/alvarezjuan/.s3cfg"
+ORIGIN_PATH="/home/alvarezjuan/www/lp_python_apps/afp-laprensa/afp/espanol/edicion-conjunta/mundo/"
+
+DB_DATABASE="laprensa-test"
+DB_USER="juancarlos"
+DB_PASSWORD="fr1ck0ff"
+DB_HOST="ec2-54-213-7-229.us-west-2.compute.amazonaws.com"
